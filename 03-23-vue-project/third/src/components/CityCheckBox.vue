407b3aad7aa738c49571d83780cd67c3930f013c
your name <1642984594@qq.com>
2019-05-11 22:57:19 +0800

    增加复选框，文本框

--- 03-23-vue-project/third/src/components/CityCheckBox.vue
+++ 03-23-vue-project/third/src/components/CityCheckBox.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
+    <div>选择网站<br/>
       <input type='checkbox' class='input-checkbox'  @click='checkedAll()'>全选
-      <div v-for='(fruit, index) in fruits' :key="index">
+      <div v-for='(webSite, index) in webSites' :key="index">
           <!--判断fruitIds是否包含当前fruit，fruitIds.indexOf(fruit.fruitId)返回包含fruit的下标, 若不包含则返回-1-->
-         <input type='checkbox' :checked="fruitIds.indexOf(fruit.fruitId)>=0" name='checkboxinput' class='input-checkbox' @click='checkedOne(fruit.id)'>
-         {{fruit.value}}
+         <input type='checkbox' :checked="webSiteIds.indexOf(webSite.id)>=0" name='checkboxinput' class='input-checkbox'
+          @click='checkedOne(webSite.id)' >
+         {{webSite.name}}
       </div>
-      <!--默认删除按钮不能点击, 当选中某一checkbox时可以删除-->
-      <button :disabled="!fruitIds.length>0" value="Delete" @click="deleteFruits()">Delete</button>
+      <input placeholder="请输入查询城市" v-model='cityName' @keyup.enter="addCity"/>
+      <button value="add" @click="addCity">添加城市</button>
+      <button value="add" @click="resetCity">清空城市</button>
+      <br/>
+      <div v-for='(city, index) in cities' :key="index">
+        <li>{{city}}</li>
+      </div>
+      <input placeholder="职位关键字" v-model='jobName' />
+      <br/>
+      <button :disabled="!webSites.length>0" value="open" @click="openWebPage()">开启爬取页面</button>
+      <hr/>
     </div>
 </template>
 <script>
@@ -15,56 +25,73 @@ export default {
   name: 'CityCheckBox',
   data () {
     return {
-      fruits: [
+      webSites: [
         {
           id: '1',
-          value: '苹果'
+          name: 'Boss直聘',
+          url: 'https://www.zhipin.com/job_detail'
         },
         {
           id: '2',
-          value: '荔枝'
+          name: '智联招聘',
+          url: 'https://sou.zhaopin.com'
         },
         {
           id: '3',
-          value: '香蕉'
-        },
-        {
-          id: '4',
-          value: '火龙果'
+          name: '100offer',
+          url: 'https://cn.100offer.com/job_positions'
         }
       ],
-      fruitIds: ['1', '3', '4'],
+      webSiteIds: [],
       // 初始化全选按钮, 默认不选
-      isCheckedAll: true
+      isWebSiteCheckedAll: false,
+      cities: [],
+      cityName: '',
+      jobName: ''
     }
   },
   methods: {
-    checkedOne (fruitId) {
+    checkedOne (webSiteId) {
       console.log(3)
-      let idIndex = this.fruitIds.indexOf(fruitId)
+      let idIndex = this.webSiteIds.indexOf(webSiteId)
       if (idIndex >= 0) {
         // 如果已经包含了该id, 则去除(单选按钮由选中变为非选中状态)
-        this.fruitIds.splice(idIndex, 1)
+        this.webSiteIds.splice(idIndex, 1)
       } else {
         // 选中该checkbox
-        this.fruitIds.push(fruitId)
+        this.webSiteIds.push(webSiteId)
       }
     },
     checkedAll () {
       console.log(2)
-      this.isCheckedAll = !this.isCheckedAll
-      if (this.isCheckedAll) {
+      this.isWebSiteCheckedAll = !this.isWebSiteCheckedAll
+      if (this.isWebSiteCheckedAll) {
         // 全选时
-        this.fruitIds = []
-        this.fruits.forEach(function (fruit) {
-          this.fruitIds.push(fruit.fruitId)
+        this.webSiteIds = []
+        this.webSites.forEach(function (webSite) {
+          this.webSiteIds.push(webSite.webSiteId)
         }, this)
       } else {
-        this.fruitIds = []
+        this.webSiteIds = []
       }
+      console.log(this.webSiteIds)
     },
-    deleteFruits () {
-      console.log(1)
+    openWebPage () {
+      console.log(this.webSiteIds)
+      console.log(this.webSites)
+      this.webSites.forEach(function (webSite) {
+        let url = webSite.url + '?cityNames=' + this.cities + '&jobName=' + this.jobName
+        console.log(url)
+        this.webSiteIds.indexOf(webSite.id) >= 0 ? window.open(url) : console.log('未选中' + webSite.name)
+      }, this)
+    },
+    addCity () {
+      this.cities.push(this.cityName)
+      console.log(this.cities)
+      this.cityName = ''
+    },
+    resetCity () {
+      this.cities = []
     }
   }
 }
